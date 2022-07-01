8f507670bdf76c339106bf56fd39c2587a8c57c4
Valmor <valmorflores@gmail.com>
2022-07-01 08:28:13 -0300

    update

--- uradapiclient/app/Views/Design/head_brand.php
+++ uradapiclient/app/Views/Design/head_brand.php
@@ -2,7 +2,7 @@
     <div class="nav-item no-hover">
 
         <h6 class="title">
-            <span class="tag tag--dark text-success"> uRep</span><span class=" subtitle font-normal">
+            <span class="tag tag--dark text-success"> uRadApiClient</span><span class=" subtitle font-normal">
                 Administração</span>
         </h6>
     </div>
@@ -18,10 +18,9 @@
                 <li role="menu-item"><a href="<?php echo base_url();?>/public/dashboard/tables/<?php echo $token ?? '';?>">Dashboard</a></li>
                 <li role="menu-item"><a href="<?php echo base_url();?>/public/profile/<?php echo $token ?? '';?>">Perfil</a></li>
                 <?php if ( isProfileAdmin() ) { ?>
-                <li role="menu-item"><a href="<?php echo base_url();?>/public/internalusers">Usuários</a></li>
+                <li role="menu-item"><a href="<?php echo base_url();?>/public/users/<?php echo $token ?? '';?>">">Usuários</a></li>
                 <?php } ?>
-                <li role="menu-item"><a href="<?php echo base_url();?>/documents/uadm_manual.pdf"
-                        target="blank">Manual</a></li>
+                
                 <li role="menu-item"><a href="<?php echo base_url();?>/public/about/index/<?php echo $token ?? '';?>">Sobre</a></li>
                 <li class="dropdown-menu-divider"></li>
                 <li role="menu-item"><a href="<?php echo base_url();?>/public/login/off">Sair</a></li>
